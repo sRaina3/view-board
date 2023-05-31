e9ecd82049d892647a34e95db9ae66de8d0ba78d
Saransh Raina <saranshraina1@gmail.com>
2023-05-31 18:06:14 -0500

    api integration working

--- view-board-frontend/src/App.js
+++ view-board-frontend/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css';
 
+import userService from './services/UserService'
+
 const App = () => {
   const [searchEntity, setSearchEntity] = useState("")
 
@@ -8,9 +10,20 @@ const App = () => {
     setSearchEntity(e.target.value)
   }
 
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && searchEntity.length > 2) {
+      console.log(searchEntity)
+      userService.getTitleInfo(searchEntity)
+        .then(response => {
+          console.log(response)
+        })
+    }
+  }
+
   return (
     <div className="container">
-      <input className="input-field" placeHolder={"Enter a title..."} value={searchEntity} onChange={updateSearch}/>
+      <input className="input-field" placeholder={"Enter a title..."} value={searchEntity}
+       onChange={updateSearch} onKeyDown={handleEnter}/>
     </div>
   );
 }
---
+++ view-board-frontend/src/services/UserService.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseURL = 'https://streaming-availability.p.rapidapi.com/v2/'
+const apiKey = '8ae7ce15a1mshcb425916ddfc8fap1b0a7djsnb36bc59d6a3e'
+
+const getTitleInfo = (searchEntity) => {
+  const options = {
+    method: 'GET',
+    url: `${baseURL}search/title`,
+    params: {
+      title: searchEntity,
+      country: 'us',
+      output_language: 'en'
+    },
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+    }
+  }
+  const request = axios.request(options)
+  return request.then(response => response.data)
+}
+
+const userService = {getTitleInfo}
+
+export default userService
