f33ddc9bba4ede0294b356e7427dcdcb64d2c086
Saransh Raina <saranshraina1@gmail.com>
2023-06-04 00:53:58 -0500

    removed debugging

--- view-board-frontend/src/App.js
+++ view-board-frontend/src/App.js
@@ -19,10 +19,6 @@ const App = () => {
         .then(response => {
           setSearchResults(response.result)
         })
-      userService.getServices()
-        .then(response => {
-          console.log(response)
-        })
     }
   }
 
