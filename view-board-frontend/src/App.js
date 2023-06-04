5b60cf4cb7e34d7af338352197c8058cd753b5d2
Saransh Raina <saranshraina1@gmail.com>
2023-06-03 20:37:59 -0500

    css addititions

--- view-board-frontend/src/App.js
+++ view-board-frontend/src/App.js
@@ -16,14 +16,14 @@ const App = () => {
 
   const handleEnter = (e) => {
     if (e.key === 'Enter' && searchEntity.length > 2) {
-      userService.getTitleInfo(searchEntity)
+      /*userService.getTitleInfo(searchEntity)
         .then(response => {
           setSearchResults(response.result)
-        })
-      /*userService.getServices()
+        })*/
+      userService.getServices()
         .then(response => {
           console.log(response)
-        })*/
+        })
     }
   }
 
@@ -32,16 +32,18 @@ const App = () => {
         <h1 className='gradient__text'>Find Streaming Availabilities</h1>
         <input className="input-field" placeholder={"Enter a title..."} value={searchEntity}
         onChange={updateSearch} onKeyDown={handleEnter}/>
-        {searchResults.length > 0 && (
+        {searchResults.length > 0 ? 
           <div className="search-results">
             {searchResults.map((result) => 
-              <div key={result.imdbId}>
-                <ResultDisplay result={result} />
-              </div>
+              <div key={result.imdbId}> <ResultDisplay result={result} /> </div>
             )}
+          </div> 
+          : <div>
+            <h1 className='initial-info-title'>Millions of movies and TV Shows to Explore</h1>
+            <h1 className='stream-stats'>20+ Streaming Platforms Supported</h1>
           </div>
-        )}
-        <h1 className='initial-info-title'>Millions of movies and TV Shows to Explore</h1>
+        }
+        
     </div>
   );
 }
