3c149be0aba717e3537c33157efe489c02949336
Saransh Raina <saranshraina1@gmail.com>
2023-06-06 16:21:02 -0500

    begun work on upcoming display

--- view-board-frontend/src/App.js
+++ view-board-frontend/src/App.js
@@ -1,7 +1,7 @@
 //APIs Used: 
 //https://rapidapi.com/movie-of-the-night-movie-of-the-night-default/api/streaming-availability
 //https://rapidapi.com/SAdrian/api/moviesdatabase
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 
 import userService from './services/UserService'
@@ -11,26 +11,40 @@ import ResultService from './ResultService';
 const App = () => {
   const [searchEntity, setSearchEntity] = useState("")
   const [searchResults, setSearchResults] = useState([])
+  const [upcomingResults, setUpcomingResults] = useState([])
+  const [curUpcoming, setCurUpcoming] = useState(0)
+
+  useEffect(() => {
+    userService.getUpcoming()
+      .then(response => {
+        console.log(response.results)
+        setUpcomingResults(response.results)
+      })
+  }, [])
 
   const updateSearch = (e) => {
     setSearchEntity(e.target.value)
   }
 
   const handleEnter = (e) => {
-    if (e.key === 'Enter' && searchEntity.length > 2) {
+    if (e.key === 'Enter' && searchEntity.length > 0) {
       /*userService.getTitleInfo(searchEntity)
         .then(response => {
           setSearchResults(response.result)
         })*/
-      userService.getUpcoming()
-        .then(response => {
-          console.log(response)
-        })
     }
   }
 
   const handleHomePage = (e) => {
     setSearchResults([])
+  }
+
+  const handleNextImage = () => {
+    if (curUpcoming < 9) {
+      setCurUpcoming(curUpcoming + 1)
+    } else {
+      setCurUpcoming(0)
+    }
   }
 
   return (
@@ -61,6 +75,13 @@ const App = () => {
               <ResultService name='PRIME'/> <ResultService name='SHOWTIME'/> <ResultService name='ZEE5'/>
           </div>
         </div>
+      }
+      {upcomingResults.length > 0 ? 
+        <div className="image-slider">
+          <img className="upcoming-image" src={upcomingResults[curUpcoming].primaryImage.url} alt='upcoming'/>
+          <button className="upcoming-button" onClick={handleNextImage}>Next</button>
+        </div> :
+        <div> </div>
       }
       <div className='footer'>
         <div className='footer-text'>My GitHub: &nbsp;
