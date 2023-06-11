3ea3335f6d0a740be46710f43b7e65481183feb4
Saransh Raina <saranshraina1@gmail.com>
2023-06-10 23:28:07 -0500

    refresh fix

--- view-board-frontend/src/index.js
+++ view-board-frontend/src/index.js
@@ -10,7 +10,7 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/view-board">
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/Upcoming' element={<Upcoming />} />
